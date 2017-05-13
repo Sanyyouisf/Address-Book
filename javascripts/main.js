b622889f63862120ee3ada63607ec17bd2840c38
Sanyyouisf <sanymamdoh@yahoo.com>
2017-05-12 19:43:23 -0500

    done with adress book

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,41 @@
-console.log("js is working");
 var app = angular.module("AddressBook",[]);
 app.controller("AdressbookCtrl",($scope)=>{
-	console.log("AdressbookCtrl");
+	$scope.showAdressbookList= false;
+	$scope.showsearchAB= false;
+	$scope.toggleAdressBook=()=>{
+		$scope.showAdressbookList= !$scope.showAdressbookList;
+	};
+	$scope.adressbookItems =
+	[
+	{
+		name:"paddy drew",
+		tel:6153545766,
+		email:"paddy@gmail.com"
+	},
+	{
+		name:"rayen stewart",
+		tel:6153347675,
+		email:"rayen@yahoo.com"
+	},
+	{
+		name:"adam chris",
+		tel:6152229898,
+		email:"adam@hotmail.com"
+	},
+	{
+		name:"jak mena",
+		tel:6159890909,
+		email:"jak@gmail.com"
+	},
+	{
+		name:"andrew solomon",
+		tel:6151215565,
+		email:"andrew@yahoo.com"
+	},
+
+	];
+
+	$scope.searchAB=()=>{
+		$scope.showsearchABList= !$scope.showsearchABList;
+	};
 });
